0e0387e3d8b8e1604e2ad2d3f0cd2bb4b4b01e65
Kjeld Schmidt <kjeld.schmidt@thoughtworks.com>
2021-12-20 07:41:26 +0100

    Day 20 part 1

--- 20-solution.py
+++ 20-solution.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-with open("20-input-small.txt", "r") as f:
+with open("20-input.txt", "r") as f:
     rules = f.readline().strip()
     rules = [0 if char == "." else 1 for char in rules.strip()]
     f.readline()
@@ -11,24 +11,28 @@ with open("20-input-small.txt", "r") as f:
         image_data.append([0 if char == "." else 1 for char in line.strip()])
 
     image_data = np.array(image_data)
+    image_data = np.pad(image_data, ((10, 10), (10, 10)))
 
 
 def area_to_dec(array):
     return int("".join(map(str, array.flatten())), 2)
 
 
-def transform(array: np.array, rules):
-    padded = np.pad(array, ((2, 2), (2, 2)))
-    n, m = padded.shape
-
-    tranformed = np.copy(padded)
+def transform(array: np.array, rules, parity):
+    n, m = array.shape
+    tranformed = np.copy(array)
     for i in range(1, n-1):
         for j in range(1, m-1):
-            area = padded[i-1:i+2, j-1:j+2]
+            area = array[i-1:i+2, j-1:j+2]
             index = area_to_dec(area)
             tranformed[i, j] = rules[index]
 
-    return tranformed[1:-1, 1:-1]
+    edge_index = 0 if parity % 2 == 0 else 511
+    tranformed[0, 0:m] = rules[edge_index]
+    tranformed[n-1, 0:m] = rules[edge_index]
+    tranformed[0:n, 0] = rules[edge_index]
+    tranformed[0:n, m-1] = rules[edge_index]
+    return tranformed
 
 
 def show_image(array):
@@ -38,15 +42,8 @@ def show_image(array):
         print()
 
 
-show_image(image_data)
-image_data = transform(image_data, rules)
-
-print()
-show_image(image_data)
-image_data = transform(image_data, rules)
-
-print()
-show_image(image_data)
+image_data = transform(image_data, rules, 0)
+image_data = transform(image_data, rules, 1)
 
 print("Solution part 1:")
 print(np.sum(image_data))
